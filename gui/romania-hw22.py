5163e927a61762958710c94464dd5146d6128615
Nikhil Raina <nxr5013@g.rit.edu>
2019-09-26 23:24:23 -0400

    import random

--- gui/romania-hw22.py
+++ gui/romania-hw22.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from search import *
 from search import breadth_first_tree_search as bfts, depth_first_tree_search as dfts, uniform_cost_search as ucs
 from gui.tree_node import *
-
+import random
 root = None
 romania_problem = None
 start = None
@@ -15,11 +15,12 @@ def bfs2():
     global romania_problem, start, goal
     romania_problem = GraphProblem(start.get(), goal.get(), romania_map)
     final_path = [start.get()]
-    result, visited_count, tree_size = bfts(romania_problem)
-    final_path += result.solution()
     totalPath = 0
-    for i in range(len(final_path) - 1):
-        totalPath += romania_map.get(final_path[i], final_path[i + 1])
+    result, visited_count, tree_size = bfts(romania_problem)
+    if result != None:
+        final_path += result.solution()
+        for i in range(len(final_path) - 1):
+            totalPath += romania_map.get(final_path[i], final_path[i + 1])
 
     return totalPath, visited_count, tree_size
 
@@ -27,11 +28,12 @@ def dfs2():
     global romania_problem, start, goal
     romania_problem = GraphProblem(start.get(), goal.get(), romania_map)
     final_path = [start.get()]
-    result, visited_count, tree_size = dfts(romania_problem)
-    final_path += result.solution()
     totalPath = 0
-    for i in range(len(final_path) - 1):
-        totalPath += romania_map.get(final_path[i], final_path[i + 1])
+    result, visited_count, tree_size = dfts(romania_problem)
+    if result != None:
+        final_path += result.solution()
+        for i in range(len(final_path) - 1):
+            totalPath += romania_map.get(final_path[i], final_path[i + 1])
 
     return totalPath, visited_count, tree_size
 
@@ -40,11 +42,12 @@ def ucs2():
     global romania_problem, start, goal
     romania_problem = GraphProblem(start.get(), goal.get(), romania_map)
     final_path = [start.get()]
-    result, visited_count, tree_size = ucs(romania_problem)
-    final_path += result.solution()
     totalPath = 0
-    for i in range(len(final_path) - 1):
-        totalPath += romania_map.get(final_path[i], final_path[i + 1])
+    result, visited_count, tree_size = ucs(romania_problem)
+    if result != None:
+        final_path += result.solution()
+        for i in range(len(final_path) - 1):
+            totalPath += romania_map.get(final_path[i], final_path[i + 1])
 
     return totalPath, visited_count, tree_size
 
@@ -60,23 +63,22 @@ def main():
                 "Hisova", "Eforie", "Vaslui", "lasi", "Neamt"]
     searches = ["Breadth-First Tree Search", "Depth-First Tree Search", "Uniform Cost Search"]
 
-    s1 = 0
-    s2 = 1
     data = dict()
+    count = 0
     for search in searches:
         data[search] = list()
         for i in range(5):
-            if s1 != s2:
-                start.set(places[2])
-                goal.set(places[11])
+            start.set(places[random.randint(0, len(places) - 1)])
+            goal.set(places[random.randint(0, len(places) - 1)])
+            if count == 0:
+                data[search].append(bfs2())
+            elif count == 1:
+                data[search].append(dfs2())
+            else:
                 data[search].append(ucs2())
-                if s2 < len(places):
-                    s2 += 1
-                else:
-                    s1 += 1
-                    s2 = 0
+        count += 1
     for key in data:
-        print(data[key])
+        print("key:", key, ":", data[key])
 
 
 if __name__ == "__main__":
